bd8817d55d124153d7deacb957f4e17b0c9fe57f
NullDefault <davisha999@gmail.com>
2019-12-03 10:57:04 -0500

    fixed structure issues

--- source/Sprites.py
+++ source/Sprites.py
@@ -1,19 +1,19 @@
 import pygame
-
+import os
 # Kenney.nl has tons of great textures for you to use - make sure to check it out!
 
 
 class Background(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("assets/background.jpg")
+        self.image = pygame.image.load('background.jpg')
         self.rect = self.image.get_rect()
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, location):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("assets/snake.png")
+        self.image = pygame.image.load('snake.png')
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
 
