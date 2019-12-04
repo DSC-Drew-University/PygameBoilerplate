510699e0edc1d129a315b9de79f6732b8a30c409
Robert Ludwick <rludwick@drew.edu>
2019-12-04 15:09:16 -0500

    Improved file structure

--- source/Sprites.py
+++ source/Sprites.py
@@ -1,19 +1,18 @@
 import pygame
-git pugit
 # Kenney.nl has tons of great textures for you to use - make sure to check it out!
 
 
 class Background(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('background.jpg')
+        self.image = pygame.image.load('source/background.jpg')
         self.rect = self.image.get_rect()
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, location):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('snake.png')
+        self.image = pygame.image.load('source/snake.png')
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
 
