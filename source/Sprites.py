7071ba0e1c77d2de0625e3dd04f1425b03c63e8d
Robert Ludwick <rludwick@drew.edu>
2019-12-04 21:51:34 -0500

    merge conflict

--- source/Sprites.py
+++ source/Sprites.py
@@ -6,26 +6,25 @@ from pygame.locals import *
 # If you want to make your own sprites - head over to www.piskelapp.com
 # it's an amazing online tool for making pixel art
 
-
-class Background(pygame.sprite.Sprite):  # Use this class to make your background - Display has all the other code you need
+class Background(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('source/background.jpg') # Change this to get the image you want
+        self.image = pygame.image.load('source/background.jpg')
         self.rect = self.image.get_rect()
 
 
-class Player(pygame.sprite.Sprite):  # You can use this class for creating your player object
+class Player(pygame.sprite.Sprite):
 
-    moving_left = False  # These boolean variables are used to know when the player is supposed to be moving or not
+    moving_left = False
     moving_right = False
     moving_up = False
     moving_down = False
 
-    speed = 6  # How many pixels the player sprite will move in a single game step
+    speed = 5  # How many pixels the player sprite will move on a button press
 
-    def process(self, input_event):  # This functions is used to respond to events such as key presses
+    def process(self, input_event):
 
-        if input_event.type == KEYDOWN:  # If a button is pressed down
+        if input_event.type == KEYDOWN:
 
             if input_event.key == K_LEFT or input_event.key == ord('a'):
                 self.moving_left = True
@@ -36,7 +35,7 @@ class Player(pygame.sprite.Sprite):  # You can use this class for creating your 
             if input_event.key == K_DOWN or input_event.key == ord('s'):
                 self.moving_down = True
 
-        if input_event.type == KEYUP:  # If a button is no longer being pressed
+        if input_event.type == KEYUP:
 
             if input_event.key == K_LEFT or input_event.key == ord('a'):
                 self.moving_left = False
@@ -47,7 +46,7 @@ class Player(pygame.sprite.Sprite):  # You can use this class for creating your 
             if input_event.key == K_DOWN or input_event.key == ord('s'):
                 self.moving_down = False
 
-    def move(self):  # This function moves the player if they are supposed to be moving
+    def move(self):
         if self.moving_left:
             self.rect.left = self.rect.left - self.speed
         if self.moving_right:
@@ -59,31 +58,7 @@ class Player(pygame.sprite.Sprite):  # You can use this class for creating your 
 
     def __init__(self, location):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('source/chicken.png')  # Change this to change the skin of the player
+        self.image = pygame.image.load('source/chicken.png')
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
 
-
-class Enemy(pygame.sprite.Sprite):  # You can use this class to create enemies
-
-    speed = 3  # This variable controls how many pixels the enemy will move in a single game step
-
-    def seek_player(self, player):  # This makes the enemy follow the player
-        target_x = player.rect.left
-        target_y = player.rect.top
-
-        if target_x > self.rect.left:
-            self.rect.left = self.rect.left + self.speed
-        elif target_x < self.rect.left:
-            self.rect.left = self.rect.left - self.speed
-
-        if target_y > self.rect.top:
-            self.rect.top = self.rect.top + self.speed
-        elif target_y < self.rect.top:
-            self.rect.top = self.rect.top - self.speed
-
-    def __init__(self, location):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('source/snake.png')  # Change this to alter the skin of the enemies
-        self.rect = self.image.get_rect()
-        self.rect.left, self.rect.top = location
